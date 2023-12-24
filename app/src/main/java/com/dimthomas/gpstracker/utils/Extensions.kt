d86d07c614fcb64d94c59039565b69250028a49c
Dmitriy Averyanov <dimthomas1989@gmail.com>
2023-12-24 12:48:28 +0300

    Added check permissions

--- app/src/main/java/com/dimthomas/gpstracker/utils/Extensions.kt
+++ app/src/main/java/com/dimthomas/gpstracker/utils/Extensions.kt
@@ -1,6 +1,8 @@
 package com.dimthomas.gpstracker.utils
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.dimthomas.gpstracker.R
 
@@ -17,4 +19,11 @@ fun AppCompatActivity.openFragment(f: Fragment) {
     supportFragmentManager
         .beginTransaction().setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
         .replace(R.id.placeHolder, f).commit()
+}
+
+fun Fragment.checkPermission(p: String): Boolean {
+    return  when (PackageManager.PERMISSION_GRANTED) {
+        ContextCompat.checkSelfPermission(activity as AppCompatActivity, p) -> true
+        else -> false
+    }
 }
