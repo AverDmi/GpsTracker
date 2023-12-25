103d4d8b978580eed656174891dfddc39c88238c
Dmitriy Averyanov <dimthomas1989@gmail.com>
2023-12-25 21:55:47 +0300

    Added LocationService

---
+++ app/src/main/java/com/dimthomas/gpstracker/location/LocationModel.kt
@@ -0,0 +1,9 @@
+package com.dimthomas.gpstracker.location
+
+import org.osmdroid.util.GeoPoint
+
+data class LocationModel(
+    val velocity: Float = 0.0f,
+    val distance: Float = 0.0f,
+    val geoPointList: ArrayList<GeoPoint>
+)
---
+++ app/src/main/java/com/dimthomas/gpstracker/location/LocationService.kt
@@ -0,0 +1,68 @@
+package com.dimthomas.gpstracker.location
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.dimthomas.gpstracker.MainActivity
+import com.dimthomas.gpstracker.R
+
+class LocationService : Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        startNotification()
+        isRunning = true
+        return START_STICKY
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        isRunning = false
+    }
+
+    private fun startNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val nChanel = NotificationChannel(
+                CHANNEL_ID,
+                "Location Service",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val nManager = getSystemService(NotificationManager::class.java) as NotificationManager
+            nManager.createNotificationChannel(nChanel)
+        }
+
+        val nIntent = Intent(this, MainActivity::class.java)
+        val pIntent = PendingIntent.getActivity(
+            this,
+            REQUEST_CODE,
+            nIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val notification = NotificationCompat.Builder(
+            this,
+            CHANNEL_ID
+        ).setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Tracker Running!")
+            .setContentIntent(pIntent)
+            .build()
+
+        startForeground(99, notification)
+    }
+
+    companion object {
+        const val CHANNEL_ID = "channel_1"
+        const val REQUEST_CODE = 10
+        var isRunning = false
+    }
+}
